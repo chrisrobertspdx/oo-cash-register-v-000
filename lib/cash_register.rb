6f8d9a432c3143b1677511182771c0d78554dac6
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-28 01:33:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title,price,quantity=1)
     self.total = self.total + (price * quantity)
-    quantity.times do 
+    quantity.times do
       self.all << title
     end
   end
